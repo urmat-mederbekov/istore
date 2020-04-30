0023bb69cc995ed8527ee5544a8dfe01dfbd9039
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-04-30 18:53:23 +0600

    application added

---
+++ src/main/java/kg/attractor/istore/IstoreApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.istore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IstoreApplication.class, args);
+    }
+
+}
