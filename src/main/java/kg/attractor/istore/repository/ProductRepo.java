83236d5e6ecfff3a40ac1c041f75a4c144adf4a2
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-05-09 00:07:31 +0600

    find by type id added

--- src/main/java/kg/attractor/istore/repository/ProductRepo.java
+++ src/main/java/kg/attractor/istore/repository/ProductRepo.java
@@ -1,8 +1,13 @@
 package kg.attractor.istore.repository;
 
 import kg.attractor.istore.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
+    Page<Product> findAllByTypeId(Integer type_id, Pageable pageable);
+
+    Page<Product> findAllByNameContainingOrDescriptionContaining(String name, String description, Pageable pageable);
 }
