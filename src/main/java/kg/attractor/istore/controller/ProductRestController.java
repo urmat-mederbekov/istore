0625a4c05937e6b415dd8e833e1a0656c19986a3
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-05-19 23:21:54 +0600

    some controllers moved to frondend directory

--- src/main/java/kg/attractor/istore/controller/ProductRestController.java
+++ src/main/java/kg/attractor/istore/controller/ProductRestController.java
@@ -3,7 +3,6 @@ package kg.attractor.istore.controller;
 import kg.attractor.istore.dto.ProductDTO;
 import kg.attractor.istore.service.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
