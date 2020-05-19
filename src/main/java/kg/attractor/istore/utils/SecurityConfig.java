338d5989a9af767e69555905d25d20e5db83d72b
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-05-19 23:23:58 +0600

    new url is closed for some

--- src/main/java/kg/attractor/istore/utils/SecurityConfig.java
+++ src/main/java/kg/attractor/istore/utils/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true);
 
         http.authorizeRequests()
-                .antMatchers("/profile", "/cart")
+                .antMatchers("/profile", "/cart", "/purchases")
                 .authenticated();
 
         http.authorizeRequests()
