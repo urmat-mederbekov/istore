8bf87c03871d43eaebf38e3f51c6a1d8669ae7f2
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-05-21 18:02:20 +0600

    localeConfig added

---
+++ src/main/java/kg/attractor/istore/utils/LocaleConfig.java
@@ -0,0 +1,28 @@
+package kg.attractor.istore.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor() {
+        var loc = new LocaleChangeInterceptor();
+        loc.setParamName("lang");
+        return loc;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+}
